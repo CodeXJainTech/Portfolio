7ca14e0e0f8bb2f152375f5db80f0f1456528e23
Anchal Jain <anchaljain1106@gmail.com>
2026-06-23 16:52:57 +0530

    update: CV

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -69,7 +69,7 @@ const About = () => {
 
           {/* ------------ View CV Button ------------ */}
           <a
-            href="https://drive.google.com/file/d/1flfZohrecrI8eKK6mbXbu3Ux5YaNg6AY/view?usp=sharing"
+            href="https://drive.google.com/file/d/1gN6W49yRAEnAZffjWA1mYpis9ycLYZmj/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
